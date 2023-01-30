05d0772f67d0900c81f0ff3402b69acece14565b
Diego Becerra <dabc2703@gmail.com>
2023-01-30 18:51:15 -0500

    first commit

---
+++ src/utils/move.util.ts
@@ -0,0 +1,33 @@
+interface MoveParams {
+  array: any[];
+  moveIndex: number;
+  toIndex: number;
+}
+
+const move = ({ array, moveIndex, toIndex }: MoveParams): any[] => {
+  const item = array[moveIndex];
+  const length = array.length;
+  const diff = moveIndex - toIndex;
+
+  if (diff > 0) {
+    // move left
+    return [
+      ...array.slice(0, toIndex),
+      item,
+      ...array.slice(toIndex, moveIndex),
+      ...array.slice(moveIndex + 1, length)
+    ];
+  } else if (diff < 0) {
+    // move right
+    const targetIndex = toIndex + 1;
+    return [
+      ...array.slice(0, moveIndex),
+      ...array.slice(moveIndex + 1, targetIndex),
+      item,
+      ...array.slice(targetIndex, length)
+    ];
+  }
+  return array;
+};
+
+export default move;
